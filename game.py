88b7fd59912625610b502a0bd6734213d2edf9a4
ArturGib <Artur.Gibadullin@frtk.ru>
2022-12-29 15:36:33 +0300

    Functions replaced from "game" to "game_v2"

--- game.py
+++ game.py
@@ -1,47 +1,26 @@
+#Game - "Guess the number"
+
 import numpy as np
-
-
-    
-def predict_random(number:int = 1) -> int:
-    """Возвращает число попыток угадывания случайного числа number
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Номер попытки с которой удалось угадать число
-    """    
-    count = 0
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)
-        if predict_number == number:
-            return count
-        
-
-def score_game(predict_random) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает 
-    алгоритм
-
-    Args:
-        predict_random (_type_): _description_
-
-    Returns:
-        int: _description_
-    """    
-    
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(10) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(predict_random(number))
-
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
 
 #RUN 
 if __name__ == "__main__":
-    score_game(predict_random)
+    
+    #score_game(predict_random)
+    
+    number = np.random.randint(1, 101) #think a number
+
+    #Number of try
+    count = 0
+
+    while True:
+        count+=1
+        predict_number = int(input("Guess the number from 1 to 100\n"))
+        
+        if predict_number > number:
+            print("Number should be less!")
+        elif predict_number < number:
+            print("Number should be greater!")
+        else:
+            print(f"You find number! It is {number}. You tried {count} times")
+            break #game over
+        
